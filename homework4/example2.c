5c982f9f5b2bcba717155206fab6ec1e19208d5e
Temur  Mannonov <t.mannonov@gmail.com>
2021-03-20 20:44:12 +0500

    fix

--- homework4/example2.c
+++ homework4/example2.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <wiringPi.h>
-#include <signal.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <softPwm.h>
 
 #define IN1_PIN 1
 #define IN2_PIN 4
 #define IN3_PIN 5
 #define IN4_PIN 6
+
+#define MAX_SPEED 50
+#define MIN_SPEED 0
 
 #define TRIG_PIN 28
 #define ECHO_PIN 29
@@ -20,9 +24,10 @@ void signalHandler(int signal);
 
 int main(void)
 {
-    if(wiringPiSetup() == -1)
+    if(wiringPiSetup() == -1){
         return 0;
-    
+    }
+
     pinMode(TRIG_PIN, OUTPUT);
     pinMode(ECHO_PIN, INPUT);
 
@@ -30,27 +35,57 @@ int main(void)
     initDCMotor();
 
     int distance;
-    
     while(1)
-    {  
+    {
         distance = getDistance();
 	    printf("Distance %dcm\n", distance);
-        if (distance < 50) {
-            goBackward();
-	    delay(1000);
+        if(distance < 50) {
+            goBackward()
         } else {
             goForward();
         }
     }
 
- 
-
- 
-   
     return 0;
 }
 
+void initDCMotor()
+{
+    pinMode(IN1_PIN, SOFT_PWM_OUTPUT);
+    pinMode(IN2_PIN, SOFT_PWM_OUTPUT);
+    pinMode(IN3_PIN, SOFT_PWM_OUTPUT);
+    pinMode(IN4_PIN, SOFT_PWM_OUTPUT);
 
+    softPwmCreate(IN1_PIN, MIN_SPEED, MAX_SPEED);
+    softPwmCreate(IN2_PIN, MIN_SPEED, MAX_SPEED);
+    softPwmCreate(IN3_PIN, MIN_SPEED, MAX_SPEED);
+    softPwmCreate(IN4_PIN, MIN_SPEED, MAX_SPEED);
+}
+
+
+void goForward()
+{
+    softPwmWrite(IN1_PIN, MAX_SPEED);
+    softPwmWrite(IN2_PIN, LOW);
+    softPwmWrite(IN3_PIN, MAX_SPEED);
+    softPwmWrite(IN4_PIN, LOW);
+}
+
+void goBackward()
+{
+    softPwmWrite(IN1_PIN, LOW);
+    softPwmWrite(IN2_PIN, MAX_SPEED);
+    softPwmWrite(IN3_PIN, LOW);
+    softPwmWrite(IN4_PIN, MAX_SPEED);
+}
+
+void stopDCMotor()
+{
+    digitalWrite(IN1_PIN, LOW);
+    digitalWrite(IN2_PIN, LOW);
+    digitalWrite(IN3_PIN, LOW);
+    digitalWrite(IN4_PIN, LOW);
+}
 
 void signalHandler(int signal)
 {
@@ -62,7 +97,7 @@ int getDistance()
 {
     int start_time=0, end_time=0;
     float distance=0;
-    
+
     digitalWrite(TRIG_PIN, LOW);
     delay(500);
     digitalWrite(TRIG_PIN, HIGH);
@@ -72,40 +107,8 @@ int getDistance()
         start_time = micros();
     while (digitalRead(ECHO_PIN) == 1);
         end_time = micros();
-    
+
     distance = (end_time - start_time) / 29. / 2.;
     return (int)distance;
 }
 
-void initDCMotor()
-{
-    digitalWrite(IN1_PIN, HIGH);
-    digitalWrite(IN2_PIN, HIGH);
-    digitalWrite(IN3_PIN, HIGH);
-    digitalWrite(IN4_PIN, HIGH);
-}
-
-void goForward()
-{
-    digitalWrite(IN1_PIN, HIGH);
-    digitalWrite(IN2_PIN, LOW);
-    digitalWrite(IN3_PIN, HIGH);
-    digitalWrite(IN4_PIN, LOW);
-}
-
-
-void goBackward()
-{
-    digitalWrite(IN1_PIN, LOW);
-    digitalWrite(IN2_PIN, HIGH);
-    digitalWrite(IN3_PIN, LOW);
-    digitalWrite(IN4_PIN, HIGH);
-}
-
-void stopDCMotor()
-{
-    digitalWrite(IN1_PIN, LOW);
-    digitalWrite(IN2_PIN, LOW);
-    digitalWrite(IN3_PIN, LOW);
-    digitalWrite(IN4_PIN, LOW);
-}
